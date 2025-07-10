d6d3a32bc3ce6c2486c6259856e766a0d5f3ff87
michaelcrg <michaelchiereghin>
2025-07-10 13:48:35 +0200

    Delete start

--- src/components/ButtonContainer.jsx
+++ src/components/ButtonContainer.jsx
@@ -1,10 +1,8 @@
 import { ResetButton } from "./ResetButton";
-import { StartButton } from "./StartButton";
 
 export function ButtonContainer() {
   return (
     <div className="flex justify-evenly items-evenly  absolute bottom-2 w-[100%] h-[30%]">
-      <StartButton />
       <ResetButton />
     </div>
   );
